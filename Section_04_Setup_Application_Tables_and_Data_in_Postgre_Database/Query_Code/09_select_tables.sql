767603731a3bd4fec608230973c74b6fec7e9c66
Romulo Vieira <romulo.vieira777@gmail.com>
2024-08-19 11:11:47 -0300

    Setup Application Tables and Data in Postgre Database

---
+++ Section_04_Setup_Application_Tables_and_Data_in_Postgre_Database/Query_Code/09_select_tables.sql
@@ -0,0 +1,34 @@
+SELECT
+    *
+FROM
+    departments;
+
+
+SELECT
+    *
+FROM
+    categories;
+
+
+SELECT
+    *
+FROM
+    products;
+
+
+SELECT
+    *
+FROM
+    customers;
+
+
+SELECT
+    *
+FROM
+    orders;
+
+
+SELECT
+    *
+FROM
+    order_items;
